49cf802b27b3f5307bd1535b1ddae058692c2139
sktkddn777 <sktkddn777@naver.com>
2021-11-12 12:27:35 +0900

    11/12

--- programmers/level3/풍선터트리기.py
+++ programmers/level3/풍선터트리기.py
@@ -1,23 +1,25 @@
 def solution(a):
-  balloon_dic = dict.fromkeys(a, 0)
-  stack = []
+    balloon_dic = dict.fromkeys(a, 0)
+    stack = []
 
-  for key in balloon_dic.keys():
-    if len(stack) == 0:
-      stack.append(key)
+    for key in balloon_dic.keys():
+        if len(stack) == 0:
+            stack.append(key)
 
-    else:
-      if key > stack[-1]:
-        balloon_dic[key] += 1
-      else:
-        while key > stack[-1]:
-          balloon_dic[stack[-1]] += 1
-          if balloon_dic[stack[-1]] == 2:
-            stack.pop()
-          if len(stack) == 0:
-            break
-      
-      stack.append(key)
-  print(stack)
+        else:
+            if key > stack[-1]:
+                balloon_dic[key] += 1
+            else:
+                while len(stack) > 0:
+                    if key > stack[-1]:
+                        balloon_dic[key] += 1
+                        break
+                    else:
+                        balloon_dic[stack[-1]] += 1
+                        if balloon_dic[stack[-1]] == 2:
+                            stack.pop()
+                        else:
+                            break
 
-solution([-16,27,65,-2,58,-92,-71,-68,-61,-33])
+            stack.append(key)
+    return len(stack)
