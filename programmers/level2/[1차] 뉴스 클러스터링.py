fd5a9a7cadb9a961be9038cc8fb0f93f5c01f035
sktkddn777 <sktkddn777@naver.com>
2021-11-15 01:23:15 +0900

    11/15

--- programmers/level2/[1차] 뉴스 클러스터링.py
+++ programmers/level2/[1차] 뉴스 클러스터링.py
@@ -28,3 +28,42 @@ def solution(str1, str2):
     return int(len(intersection) / len(union) * 65536)
 
 # Counter에 대해 알게 되었다.
+
+# 11/15 Counter를 안쓰고 풀어봤다. Union intersection의 중복이 허용됨을 문제를 통해 확인 잘 해줘야 한다. 
+# 이 부분 때문에 시간 좀 잡아 먹었다.
+
+'''
+
+def Jacard(str1, str2):
+    lst1, lst2 = [], []
+
+    for i in range(len(str1)-1):
+        if str1[i].isalpha() and str1[i+1].isalpha():
+            lst1.append(str1[i:i+2])
+    for i in range(len(str2) - 1):
+        if str2[i].isalpha() and str2[i+1].isalpha():
+            lst2.append(str2[i:i+2])
+
+    union, intersection = [], []
+    hap, gyo = (set(lst1) | set(lst2)), (set(lst1) & set(lst2))
+    for i in gyo:
+        for _ in range(min(lst1.count(i), lst2.count(i))):
+            intersection.append(i)
+    for i in hap:
+        for _ in range(max(lst1.count(i), lst2.count(i))):
+            union.append(i)
+
+    if len(union) == 0:
+        return 1
+
+    return len(intersection)/len(union)
+
+
+def solution(str1, str2):
+    str1, str2 = str1.upper(), str2.upper()
+
+    n = Jacard(str1, str2)
+
+    return int(n * 65536)
+
+'''
