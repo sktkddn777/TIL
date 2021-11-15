f26a8c34910fe9a48ed825eff332c1d56fa64c20
sktkddn777 <sktkddn777@naver.com>
2021-11-15 11:53:43 +0900

    11/15

--- programmers/level2/2개 이하로 다른 비트.py
+++ programmers/level2/2개 이하로 다른 비트.py
@@ -27,3 +27,29 @@ def solution(numbers):
         answer.append(find_num(number))
 
     return answer
+
+# 11/15 다시 풀어봤는데 규칙을 찾지 못해서 다시 풀이를 보고 풀었다.
+'''
+def find_num(num):
+    bin_num = list(bin(num)[::-1][:-2])
+
+    if num % 2 == 0:
+        bin_num[0] = '1'
+    else:
+        try:
+            idx=bin_num.index('0')
+            bin_num[idx] = '1'
+            bin_num[idx-1] = '0'
+        except ValueError:
+            bin_num.append('1')
+            bin_num[-2] = '0'
+
+    return ''.join(bin_num[::-1])
+
+def solution(numbers):
+    result = []
+    for number in numbers:
+        a = int(find_num(number),2)
+        result.append(a)
+    return result
+'''
