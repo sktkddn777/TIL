81b63c8334c831bb9ba3b3005fbc8f0c3e5e42b3
sktkddn777 <sktkddn777@naver.com>
2021-11-22 21:49:59 +0900

    11/22

--- baekjoon/greedy/빵집.py
+++ baekjoon/greedy/빵집.py
@@ -1,15 +1,34 @@
-from collections import deque
+'''
+dfs를 통해 방문 방법을 탐색한다.
+
+처음에 생각을 잘못해 정답 코드를 참고하였다.
+'''
 
 R, C = map(int, input().split())
 map_lst = []
 for _ in range(R):
   map_lst.append(list(input()))
 
-dx = [0, -1, 1]
-dy = [1, 1, 1]
+visited = [[0] * C for _ in range(R)]
 
-for i in range(R):
-  queue = deque([i,0])
+dx = [-1, 0, 1]
 
-  while queue:
-    
+def dfs(x, y):
+  if y == C-1:
+    return True
+  
+  for i in range(3):
+    tx = x + dx[i]
+    if 0<= tx < R:
+      if visited[tx][y+1] == 0 and map_lst[tx][y+1] == '.':
+        visited[tx][y+1] = 1
+        if dfs(tx, y+1):
+          return True
+  return False
+
+total = 0
+for x in range(R):
+  if dfs(x, 0):
+    total += 1
+
+print(total)
