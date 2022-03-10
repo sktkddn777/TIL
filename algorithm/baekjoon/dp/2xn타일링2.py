15a17e7a9c513ccfabd1a007bfe3dd52d634e345
sktkddn777 <sktkddn777@naver.com>
2022-03-11 01:03:23 +0900

    solving dp 3/11

---
+++ algorithm/baekjoon/dp/2xn타일링2.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+dp = [0] * (N+1)
+
+if N == 1:
+  print(1)
+elif N == 2:
+  print(2)
+else:
+  dp[1], dp[2] = 1, 3
+  for i in range(3, N+1):
+    dp[i] = (dp[i-1] + 2 * dp[i-2]) % 10007
+print(dp[N])
