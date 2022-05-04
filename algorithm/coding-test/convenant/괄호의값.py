cb83ba0529dff02c0ce6db14c84f57d89636817e
sktkddn777 <sktkddn777@naver.com>
2022-05-04 16:51:37 +0900

    baekjoon solve 5/4

--- algorithm/coding-test/convenant/괄호의값.py
+++ algorithm/coding-test/convenant/괄호의값.py
@@ -29,3 +29,49 @@ for i in range(len(g_lst)):
         stack.pop()
 
 print(res)
+
+# lst = list(input())
+
+# def is_correct(lst):
+#     res = 0
+#     stack = []
+
+#     tmp = 1
+#     flg = 1
+
+#     for x in lst:
+#         if x == '(' or x == '[':
+#             if x == '(':
+#                 tmp *= 2
+
+#             elif x == '[':
+#                 tmp *= 3
+#             stack.append(x)
+#             flg = 1
+
+#         else:
+#             if not stack:
+#                 return 0
+
+#             if x == ')':
+#                 if stack[-1] == '(' and flg:
+#                     res += tmp
+#                     flg = 0
+#                 elif stack[-1] == '[':
+#                     return 0
+#                 tmp = tmp // 2
+#             elif x == ']':
+#                 if stack[-1] == '[' and flg:
+#                     res += tmp
+#                     flg = 0
+#                 elif stack[-1] == '(':
+#                     return 0
+#                 tmp = tmp // 3
+#             stack.pop()
+
+#     if stack:
+#         return 0
+
+#     return res
+
+# print(is_correct(lst))
--- algorithm/coding-test/convenant/사탕게임.py
+++ algorithm/coding-test/convenant/사탕게임.py
@@ -1,35 +1,50 @@
-from collections import Counter
-
 N = int(input())
-
 candy_lst = []
+for _ in range(N):
+    candy_lst.append(list(input()))
 candy_lst_rotate = []
-for i in range(N):
-    lst = list(input())
-    candy_lst.append(lst)
-
 candy_lst_rotate = list(zip(*candy_lst))
 candy_lst_rotate = [list(x) for x in candy_lst_rotate]
 
-res = []
-for i in range(N):
-    for j in range(N):
-        tmp1 = candy_lst[i][j]
-        tmp2 = candy_lst_rotate[i][j]
-        res.append(max(Counter(candy_lst[i]).values()))
-        res.append(max(Counter(candy_lst_rotate[i]).values()))
-        if i != N-1:
-            candy_lst[i][j] = candy_lst[i+1][j]
-            candy_lst_rotate[i][j] = candy_lst_rotate[i+1][j]
-            res.append(max(Counter(candy_lst[i]).values()))
-            res.append(max(Counter(candy_lst_rotate[i]).values()))
+def count_logest(lst):
+    count = 1
+    max_c = 0
 
-        if i != 0:
-            candy_lst[i][j] = candy_lst[i-1][j]
-            candy_lst_rotate[i][j] = candy_lst_rotate[i-1][j]
-            res.append(max(Counter(candy_lst[i]).values()))
-            res.append(max(Counter(candy_lst_rotate[i]).values()))
-        candy_lst[i][j] = tmp1
-        candy_lst_rotate[i][j] = tmp2
+    for i in range(len(lst)-1):
+        if lst[i] == lst[i+1]:
+            count += 1
+        else:
+            max_c = max(max_c, count)
+            count = 1
 
-print(max(res))
+    max_c = max(max_c, count)
+    return max_c
+
+def detect(location):
+    x, y = location
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    res = 0
+
+    
+    for i in range(4):
+        tx = x + dx[i]
+        ty = y + dy[i]
+        if 0 <= tx < N and 0 <= ty < N:
+            if candy_lst[x][y] != candy_lst[tx][ty]:
+                candy_lst[x][y], candy_lst[tx][ty] = candy_lst[tx][ty], candy_lst[x][y]
+                res = max(res, count_logest(candy_lst[x]))
+                candy_lst[tx][ty], candy_lst[x][y] = candy_lst[x][y], candy_lst[tx][ty]
+            if candy_lst_rotate[x][y] != candy_lst_rotate[tx][ty]:
+                candy_lst_rotate[x][y], candy_lst_rotate[tx][ty] = candy_lst_rotate[tx][ty], candy_lst_rotate[x][y]
+                res = max(res, count_logest(candy_lst_rotate[x]))
+                candy_lst_rotate[tx][ty], candy_lst_rotate[x][y] = candy_lst_rotate[x][y], candy_lst_rotate[tx][ty]
+    return res
+res = 0
+for x in range(N):
+    for y in range(N):
+        res = max(res, count_logest(candy_lst[x]))
+        res = max(res, count_logest(candy_lst_rotate[y]))
+        res = max(res, detect((x, y)))
+
+print(res)
